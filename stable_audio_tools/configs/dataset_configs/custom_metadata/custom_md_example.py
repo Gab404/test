7cd2af668f2aacfcbb791c5698ad32cd359aabac
Gabriel Guiet-Dupré <gabriel.guiet-dupre@epitech.eu>
2025-07-29 20:47:02 +0200

    updates

--- stable_audio_tools/configs/dataset_configs/custom_metadata/custom_md_example.py
+++ stable_audio_tools/configs/dataset_configs/custom_metadata/custom_md_example.py
@@ -1,4 +1,20 @@
-def get_custom_metadata(info, audio):
+import json
+import os
 
-    # Use relative path as the prompt
-    return {"prompt": info["relpath"]}
+
+def get_custom_metadata(info, audio):
+    jsonl_file_path = r"/content/drive/MyDrive/dataset/tags.jsonl"
+    prompt = ""
+
+    # extraire le chiffre à partir de "audio/4.wav"
+    filename = os.path.basename(info["test"])      # -> "4.wav"
+    search_path = os.path.splitext(filename)[0]   # -> "4"
+
+    with open(jsonl_file_path, "r", encoding="utf-8") as jsonl_file:
+        for line in jsonl_file:
+            entry = json.loads(line)
+            if entry["filepath"] == search_path:
+                prompt = entry.get("prompt", "")
+                break
+
+    return {"prompt": prompt}
