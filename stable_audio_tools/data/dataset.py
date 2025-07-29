bd05b19ac40117a9b37e3515171895f8ef2e8916
Gabriel Guiet-Dupré <gabriel.guiet-dupre@epitech.eu>
2025-07-29 21:14:43 +0200

    updates

--- stable_audio_tools/data/dataset.py
+++ stable_audio_tools/data/dataset.py
@@ -225,7 +225,7 @@ class SampleDataset(torch.utils.data.Dataset):
             for root_path in self.root_paths:
                 if root_path in audio_filename:
                     info["relpath"] = path.relpath(audio_filename, root_path)
-
+            info["test"] = os.path.join(*audio_filename.split(os.sep)[-2:])
             info["timestamps"] = (t_start, t_end)
             info["seconds_start"] = seconds_start
             info["seconds_total"] = seconds_total
